2312614104529950a6f47b8be02076340b6c60bb
AcolombianBoy <127261875+AcolombianBoy@users.noreply.github.com>
2025-04-25 17:08:09 -0500

    Reestructurar formularios de inicio de sesión y registro en index.html, eliminar estilos innecesarios y agregar nuevos archivos de estilos.

---
+++ MTC-Assistance/scripts/alternate-user-form.js
@@ -0,0 +1,6 @@
+const LoginForm = document.getElementById("login-form");
+const RegisterForm = document.getElementById("register-form");
+function showLoginForm() {
+    RegisterForm.style.visibility = "visible";
+    LoginForm.style.visibility = "hidden";
+}
