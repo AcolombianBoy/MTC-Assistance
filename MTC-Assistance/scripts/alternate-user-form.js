a85b0fc4be34ab63f944f66232cf1df2666ad889
AcolombianBoy <127261875+AcolombianBoy@users.noreply.github.com>
2025-04-25 17:28:21 -0500

    Actualizar formularios de inicio de sesión y registro en index.html y alternate-user-form.js, corrigiendo referencias y mejorando la visibilidad de los formularios.

--- MTC-Assistance/scripts/alternate-user-form.js
+++ MTC-Assistance/scripts/alternate-user-form.js
@@ -1,6 +1,10 @@
-const LoginForm = document.getElementById("login-form");
-const RegisterForm = document.getElementById("register-form");
+const LoginForm = document.getElementById("login-container");
+const RegisterForm = document.getElementById("register-container");
 function showLoginForm() {
     RegisterForm.style.visibility = "visible";
     LoginForm.style.visibility = "hidden";
+}
+function showRegisterForm() {
+    RegisterForm.style.visibility = "hidden";
+    LoginForm.style.visibility = "visible";
 }
